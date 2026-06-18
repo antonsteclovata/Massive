a73f8bb5e2eda37f6aae3dd19c6e9f264d24c908
antonsteclovata <antosha-sidorov-2015@mail.ru>
2026-06-18 05:57:58 +0300

    Update Delete_Char.java

--- Delete_Char.java
+++ Delete_Char.java
@@ -1,4 +1,5 @@
 public class Delete_Char {
+    // Удаление пробелов из строки
     public static void main(String[] args) {
         StringBuilder str = new StringBuilder("Стихи уж точно не слова\n" +
                 "С набором рифм и расстановкой точек,\n" +
